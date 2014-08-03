a7fa037ed70624bf6754c3df731f8ae0e8626e2f
crazylzk <zhongkuilaoda@163.com>
2014-08-03 22:15:51 +0800

    Scene refine
    Scene refine

--- DxGene/DXGScene.h
+++ DxGene/DXGScene.h
@@ -1,6 +1,7 @@
 #ifndef DXGSCENE_H
 #define DXGSCENE_H
-#include<vector>
+#include <vector>
+#include <map>
 
 class CDXGSceneObject;
 class IDXGSceneVisitor
@@ -11,23 +12,31 @@ public:
 };
 class CDXGScene
 {
-private:
-	std::vector<CDXGSceneObject *> m_Objs;
 public:
 	typedef CDXGSceneObject * CREATOR_FUNC(void * vContex);
+	typedef std::vector<CDXGSceneObject *> SOBJ_VEC;
+	typedef std::map<const char *, CREATOR_FUNC> CREATOR_MAP;
+private:
+	SOBJ_VEC m_Objs;
+public:
+	
 public:
 	CDXGScene(void);
 	~CDXGScene(void);
 	void update();
 	void onVisit(IDXGSceneVisitor * vVistor);
+	
 	CDXGSceneObject * getSceneObject( const char * vName);
+	CDXGSceneObject * getSceneRoot();
 
 public:
 	// factor method
 	static bool registerClass(const char *vClassName, CREATOR_FUNC vFunc);
 	static void unRegsiterClass(const char *vClassName);
+	static CDXGSceneObject * create(CDXGScene *scene, const char *vClassName, void *vContex);
 
-	static CDXGSceneObject * create(const char *vClassName, void *vContex);
+private:
+	static CREATOR_MAP creatorMap;
 
 };
 
